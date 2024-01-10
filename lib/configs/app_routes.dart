19addc07792fd26304cb2ef9f55d93c862005f43
Hestice <custillano@gmail.com>
2024-01-10 20:23:15 +0800

    bug fix

--- lib/configs/app_routes.dart
+++ lib/configs/app_routes.dart
@@ -1,15 +1,16 @@
 import 'package:go_router/go_router.dart';
 import 'package:crs_undergrad/screens/login_screen.dart';
+import 'package:crs_undergrad/screens/landing_screen.dart';
 
 var router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const LoginScreen(),
+      builder: (context, state) => LandingScreen(),
     ),
-    GoRoute(
-      path: '/home',
-      // home screen route here
-    ),
+    // GoRoute(
+    //   path: '/home',
+    //   // home screen route here
+    // ),
   ],
 );
